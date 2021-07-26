53fa556fe2b19a3c59f8e47199562e55e08a9943
Regina Berger <rbr@aformatik.de>
2021-07-26 17:58:43 +0200

    Auflisten der vorhandenen Produkte

--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from 'src/app/shared/services/products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
+  constructor(private productsService: ProductsService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  /**
+   * @returns die Anzahl der Produkte
+   */
+  public getProductCount() {
+    return this.productsService.getProductCount();
   }
 
+  /**
+   * @returns alle Produkte als Observable
+   */
+  public getAllProducts$() {
+    return this.productsService.getAllProducts$();
+  }
 }
