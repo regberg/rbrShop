a64f2f23dbd023766dc6f4c6b58230abe3a033ae
Regina Berger <rbr@aformatik.de>
2021-08-03 17:51:41 +0200

    Erweiterung Anzeigen der Produktdetails

--- src/app/features/order-product-detail/order-product-detail.component.ts
+++ src/app/features/order-product-detail/order-product-detail.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/shared/interfaces/product.interface';
+import { ProductsService } from 'src/app/shared/services/products.service';
 
 @Component({
   selector: 'app-order-product-detail',
   templateUrl: './order-product-detail.component.html',
-  styleUrls: ['./order-product-detail.component.css']
+  styleUrls: ['./order-product-detail.component.css'],
 })
 export class OrderProductDetailComponent implements OnInit {
+  public product: Product;
 
-  constructor() { }
+  constructor(
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly productsService: ProductsService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.activatedRoute.params.subscribe((params) => {
+      this.product = this.productsService.getProductById(parseFloat(params.id));
+    });
   }
-
 }
